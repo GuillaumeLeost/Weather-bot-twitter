21a9a2901225958239a926fa562d6c75ef6a92e1
Guillaume LEOST <38223058+GuillaumeLeost@users.noreply.github.com>
2018-07-10 12:27:39 +0200

    Update crontemp.py

--- crontemp.py
+++ crontemp.py
@@ -1,7 +1,7 @@
 from crontab import CronTab
 
 cron = CronTab(user='pi')
-job = cron.new(command='python weather.py')
+job = cron.new(command='python weather-request.py')
 job.minute.every(30)
 
 cron.write()
